842f2beadd89f3574f5ed23e8e4ecb51dd4c9d55
JCharlesCM-hub <jcharlescm@hotmail.com>
2024-09-30 17:03:53 -0300

    Alteração de tudo 01

--- cart/cart.py
+++ cart/cart.py
@@ -10,13 +10,22 @@ class Cart():
         # if se user is new, no session key! create one!
         # se o usuário for novo, nenhuma chave de sessão! crie uma!
         if 'session_key' not in request.session:
-            cart = self.session['session_key'] = {}
+            cart = self.session['session_key'] = {} 
         
         # Make sure cart is available on all pages of site
         # Certifique-se de que o carrinho esteja disponível em todas as páginas do site
         self.cart = cart
 
+    def add(self, product):
+        product_id = str(product.id)
 
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
 
 
 
