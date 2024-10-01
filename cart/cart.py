3583ce68adb571a668cef49dc538608f097a746d
JCharlesCM-hub <jcharlescm@hotmail.com>
2024-10-01 00:16:06 -0300

    Alteração 05

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,4 @@
+from store.models import Product
 
 class Cart():
     def __init__(self, request):
@@ -16,16 +17,32 @@ class Cart():
         # Certifique-se de que o carrinho esteja disponível em todas as páginas do site
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         # Logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)          
 
         self.session.modified = True
 
+    def __len__(self):
+        return len(self.cart)
+    
+    def get_prods(self):
+        # Get ids from cart
+        product_ids = self.cart.keys()
+        # Use ids to lookup products in database model
+        products = Product.objects.filter(id__in=product_ids)
 
+        # Return those looked up products
+        return products
+
+def get_quants(self):
+    quantities = self.cart
+    return quantities
 
