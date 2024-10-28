362ca162690dd353b0bcccba46502babf6da7774
JCharlesCM-hub <jcharlescm@hotmail.com>
2024-10-28 18:57:58 -0300

    Alteração do dia 28/10/2024 01

--- payment/forms.py
+++ payment/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import ShippingAddress
 
 class ShippingForm(forms.ModelForm):
-    shipping_fuul_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}), required=True)
+    shipping_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}), required=True)
     shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=True)
     shipping_address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address1'}), required=True)
     shipping_address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address2'}), required=False)  
--- payment/views.py
+++ payment/views.py
@@ -1,18 +1,109 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from payment.forms import ShippingForm, PaymentForm
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 from django.contrib import messages
+from store.models import Product
+
 
 def process_order(request):
     if request.POST:
+        # get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
         # Get Billing Info from the last page
         payment_form = PaymentForm(request.POST or None)
         # Get Shipping Session Data
         my_shipping = request.session.get('my_shipping')
-        print(my_shipping)
-        messages.success(request, "Order Placed!")
-        return redirect('home')
+        # print(my_shipping)
+
+        # gATHER oRDER iNFO
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        
+        # Create Shipping Address from Session Info
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+        amount_paid = totals
+
+        # Create an Order
+        if request.user.is_authenticated:
+            # Logged in
+            user = request.user
+            # Create Order
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            # Add order items
+            # Get the order id -- Obter o ID do pedido
+            order_id = create_order.pk
+            # Get product Info
+            for product in cart_products:
+                # Get product ID
+                product_id = product.id
+                # Get product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                # Get quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # Create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the Key
+                    del request.session[key]
+
+
+
+            messages.success(request, "Order Placed! Pedido realizado!")
+            return redirect('home')
+
+
+        else:
+             # Create Order
+            create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            # Add order item
+            # Get the order id
+            order_id = create_order.pk
+            # Get product Info
+            for product in cart_products:
+                # Get product ID
+                product_id = product.id
+                # Get product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                # Get quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # Create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the Key
+                    del request.session[key]
+            
+            messages.success(request, "Order Placed! Pedido realizado!")
+            return redirect('home')        
+        # print(shipping_address)
+
     else:
         messages.success(request, "Access Denied - Acesso Negado")
         return redirect('home')
@@ -29,6 +120,7 @@ def billing_info(request):
         # Create a Session with Shipping Info
         my_shipping = request.POST
         request.session['my_shipping'] = my_shipping
+
         # Check to see if user is logged in
         if request.user.is_authenticated:
             # Get the Billing Form
